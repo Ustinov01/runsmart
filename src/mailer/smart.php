96e67f027a8234fb072ed4c8046bb94de2c94b71
Ustinov01 <127645764+Ustinov01@users.noreply.github.com>
2023-04-08 20:23:40 +0600

    Update smart.php
    secret email

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->Host = '***';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'Lamorazmo@yandex.ru';                 // Наш логин
-$mail->Password = 'mtjepabggxlykphv';                           // Наш пароль от ящика
+$mail->Username = '***';                 // Наш логин
+$mail->Password = '***';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('Lamorazmo@yandex.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('oleg.ustinov.1993@mail.ru');     // Add a recipient
+$mail->setFrom('***', 'Pulse');   // От кого письмо 
+$mail->addAddress('***');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
